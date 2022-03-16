a885dc0df634db022ea592802918838771c3a0a0
Alberto Casanova <alberto.casanova@bonarea.com>
2022-03-16 10:13:58 +0100

    Added SweetAlert2 example

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-favorite',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
+  // Primero se ejecuta el constructor, se instancia el objeto
   constructor() { }
 
   ngOnInit(): void {
+    console.log('This is init method');
+  }
+
+  simpleAlert(){
+    Swal.fire('Hello world!');
+  }
+
+  alertWithSuccess(){
+    Swal.fire('Thank you...','You submitted successfully!','success');
+  }
+
+  confirmBox(){
+    Swal.fire({
+      title: 'Are you want to remove',
+      text: 'You will not be able to recover this file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result)=>{
+      if( result.value ){
+        Swal.fire('Deleted!','Your imaginary has been delete','success');
+      } else if( result.dismiss === Swal.DismissReason.cancel){
+        Swal.fire('Cancelled','Your imaginary file is safe','error');
+      }
+    })
+  }
+
+  errorAlert(){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong!',
+      footer: '<a href>Why do I have this issue?</a>'
+    });
   }
 
 }
